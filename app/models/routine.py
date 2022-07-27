55eeac7df2a808d2c2c8307f5b9386a833c10b10
joank <jakovacs@protonmail.com>
2022-07-27 11:29:19 -0700

    Added GET all, GET one, and POST endpoints for routines

--- app/models/routine.py
+++ app/models/routine.py
@@ -4,3 +4,11 @@ class Routine(db.Model):
     routine_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "id":self.routine_id,
+            "title":self.title,
+            "desc":self.description,
+            }
+
--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,72 @@ from app import db
 from app.models.routine import Routine
 
 
+##### TABLE OF CONTENTS #############################################
+
+#   [0] IMPORTS
+#   [1] BLUEPRINT DEFINITIONS
+#   [2] HELPER FUNCTIONS
+#   [3] ROUTINE ENDPOINTS
+#   [4] TASKS ENDPOINTS
+
+
+##### [1] BLUEPRINT DEFINITIONS #####################################
+
 routine_bp = Blueprint('routine_bp', __name__, url_prefix='/routines')
+#task_bp = Blueprint('task_bp', __name__, url_prefix='/tasks')
+
+
+##### [2] HELPER FUNCTIONS ##########################################
+
+def validate_id(object_id, object_type):
+    '''
+    Validates the routine or task based on ID and fetches the object from the database.
+        *object_id:  id of a routine or task
+        *object_type: "routine" or "task" depending on endpoint
+        OUTPUT: routine or task object fetched from database.
+    '''
+    try:
+        object_id = int(object_id)
+    except:
+        abort(make_response({"message":f"{object_type} {object_id} invalid"}, 400))
+
+    if object_type == "routine":
+        response = Routine.query.get(object_id)
+    #elif object_type == "task":
+        #response = Task.query.get(object_id)
+    if not response:
+        abort(make_response({"message":f"{object_type} {object_id} not found"}, 404))
+
+    return response
+
+
+##### [3] ROUTINE ENDPOINTS #########################################
+
+@routine_bp.route("", methods=["GET"])
+def get_all_routines():
+    routines = Routine.query.all()
+    return jsonify([routine.to_dict() for routine in routines]), 200
+
+
+@routine_bp.route("/<routine_id>", methods=["GET"])
+def get_one_routine(routine_id):
+    routine = validate_id(routine_id, "routine")
+    return jsonify(routine.to_dict()), 200
+
+
+@routine_bp.route("", methods=["POST"])
+def post_routine():
+    request_body = request.get_json()
+
+    if "title" in request_body and "description" in request_body:
+        new_routine = Routine(
+            title=request_body["title"],
+            description=request_body["description"],
+            )
+    else:
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    db.session.add(new_routine)
+    db.session.commit()
+
+    return make_response(new_routine.to_dict(), 201)
