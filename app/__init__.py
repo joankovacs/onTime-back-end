153c954de0bc768d8fd9f22ff745e2712c49bbdd
Julie <jawarren89@gmail.com>
2022-07-28 14:34:05 -0700

    update __init__ file for missing imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/ontime_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -20,4 +24,5 @@ def create_app(test_config=None):
     from .routes import task_bp
     app.register_blueprint(task_bp)
 
+    CORS(app)
     return app
