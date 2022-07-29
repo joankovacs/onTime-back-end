50f20fc7523aa4c37c97e24b64b1e889cecf7517
joank <jakovacs@protonmail.com>
2022-07-29 13:39:44 -0700

    routine updates

--- app/models/routine.py
+++ app/models/routine.py
@@ -5,12 +5,12 @@ class Routine(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     destination = db.Column(db.String) #<< Location object?
+
     complete_time = db.Column(db.DateTime)
     start_time = db.Column(db.DateTime)  #Calculated from complete time
-
     total_time = db.Column(db.Integer) #<< number of minutes, calculated
-    saved = db.Column(db.Boolean) #defaults to True
 
+    saved = db.Column(db.Boolean) #defaults to True
     tasks = db.relationship("Task", back_populates="routine", lazy=True)
 
 
@@ -20,8 +20,12 @@ class Routine(db.Model):
             "title":self.title,
             "description":self.description,
             "destination": self.destination,
+
             "complete_time": self.complete_time,
-            "start_time": self.complete_time,
+            "start_time": self.start_time,
+            "total_time":self.total_time,
+
+            "saved":self.saved,
             "tasks":[task.to_routine() for task in self.tasks]
             }
             
